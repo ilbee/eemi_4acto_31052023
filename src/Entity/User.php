db463ff15dc42166c37bfd96d671cef7ee5151e7
Julien PRIGENT <julien.prigent@fnacdarty.com>
2023-05-31 12:19:57 +0200

    Add prepersist

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[ORM\HasLifecycleCallbacks]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -38,6 +39,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
+
+	#[ORM\PrePersist]
+	public function prePersist()
+	{
+		$this->setCreatedAt(new \DateTimeImmutable());
+	}
 
     public function getId(): ?int
     {
